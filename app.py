dcaa2965c5ddeb34eb3d44b9be729905d5f45680
tina <rtinahubert@gmail.com>
2022-10-09 22:16:17 +0330

    Cross origin

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 import encodings
 from flask import Flask, jsonify
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from scraper import *
 
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 CORS(app)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 @app.route('/')
 def index():
     return "ok"
 
 @app.route('/api/')
+@cross_origin()
 def api():
     data = get_headline_midi()
     return jsonify({
